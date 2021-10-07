39e834f3540974ce36ed11b7bff3dbe5e007af7c
Aravindhan V <varavindhan2010@gmail.com>
2021-10-07 19:16:00 +0530

    MODIFIED: valdiation regex to validate first optional part if present

--- Email.js
+++ Email.js
@@ -1,3 +1,3 @@
-let emailRegex = RegExp("^abc@[a-z]+\\.[a-z]{2,3}");
-let input = "abc@bridgelabz.co";
+let emailRegex = RegExp("^abc[_+\\-.][a-z]+@[a-z]+\\.[a-z]{2,3}");
+let input = "abc_xyz@bridgelabz.co";
 console.log(emailRegex.test(input));
