d58824222a1547cd71d8aabe8a2fa8ef9eabaa8f
Aravindhan V <varavindhan2010@gmail.com>
2021-10-07 19:18:35 +0530

    MODIFIED: validation regex to validate second optional part if present

--- Email.js
+++ Email.js
@@ -1,3 +1,5 @@
-let emailRegex = RegExp("^abc[_+\\-.][a-z]+@[a-z]+\\.[a-z]{2,3}");
-let input = "abc_xyz@bridgelabz.co";
+let emailRegex = RegExp(
+  "^abc[_+\\-.][a-z]+@[a-z]+\\.[a-z]{2,3}(\\.[a-z]{2})?$"
+);
+let input = "abc_xyz@bridgelabz.co.in";
 console.log(emailRegex.test(input));
